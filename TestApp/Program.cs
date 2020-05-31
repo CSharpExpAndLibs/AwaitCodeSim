0baacf53f2f8384e7805b916046b927ea44528d6
sak52jp <charan.49jp@gmail.com>
2020-05-31 13:51:02 +0900

    戻り値がある場合の模擬コードも完成した。これで、理解はほぼ完ぺき...（かな？）

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -27,6 +27,26 @@ namespace TestApp
             Console.WriteLine("--Exit HeavyWork()--");
 
         }
+
+        public static async Task<string> HeavyWorkWithReturn()
+        {
+            Console.WriteLine("--Enter HeavyWork()--");
+
+            Console.WriteLine("--Task id={0},Thread id={1},Context={2}--",
+                Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
+                SynchronizationContext.Current);
+
+            await Task.Run(() => { Program.HeavyMethod(); });
+
+            Console.WriteLine("--Task id={0},Thread id={1},Context={2}--",
+                Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
+                SynchronizationContext.Current);
+
+            Console.WriteLine("--Exit HeavyWork()--");
+
+            return "@@@@ AwaitStructure.HeavyWorkWithReturn() Finished @@@@";
+
+        }
     }
 
     class AwaitCodeSim
@@ -70,6 +90,47 @@ namespace TestApp
 
             a();
             return lastTsk;
+
+        }
+
+        public static Task<string> HeavyWorkWithReturn()
+        {
+            Console.WriteLine("--Enter HeavyWork()--");
+
+            Console.WriteLine("--Task id={0},Thread id={1},Context={2}--",
+                Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
+                SynchronizationContext.Current);
+            var state = 0;
+            var tcs = new TaskCompletionSource<string>();
+            Task tsk = null;
+
+            void a()
+            {
+                switch (state)
+                {
+                    case 1: goto Case_1;
+                }
+
+                tsk = Task.Run(() => { Program.HeavyMethod(); });
+                tsk.ContinueWith((_) => { a(); });
+                state = 1;
+                return;
+
+            Case_1:
+                tsk.Wait();
+
+                Console.WriteLine("--Task id={0},Thread id={1},Context={2}--",
+                    Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
+                    SynchronizationContext.Current);
+
+                Console.WriteLine("--Exit HeavyWork()--");
+
+                tcs.SetResult("@@@@ AwaitCodeSim.HeavyWorkWithReturn() Finished @@@@");
+
+            }
+
+            a();
+            return tcs.Task;
 
         }
 
@@ -122,6 +183,31 @@ namespace TestApp
             Console.WriteLine("...Waiting task completion...");
             t.Wait();
             Console.WriteLine("...Task Complete...");
+
+            Console.WriteLine("===============================================");
+            string ret = null;
+            Console.WriteLine("Before call AwaitStructure.HeavyWorkWithReturn()");
+            Task<string> t2 = AwaitStructure.HeavyWorkWithReturn();
+            Console.WriteLine("Return from AwaitStructure.HeavyWorkWithReturn() [tid={0}]", t.Id);
+            Console.WriteLine("Task id={0},Thread id={1},Context={2}",
+                Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
+                SynchronizationContext.Current);
+            Console.WriteLine("...Waiting task completion...");
+            ret = t2.Result;
+            Console.WriteLine("...Task Complete:{0}...", ret);
+            Console.WriteLine("===============================================");
+
+            ret = null;
+            Console.WriteLine("Before call AwaitCodeSim.HeavyWorkWithReturn()");
+            t2 = AwaitCodeSim.HeavyWorkWithReturn();
+            Console.WriteLine("Return from AwaitCodeSim.HeavyWorkWithReturn() [tid={0}]", t.Id);
+            Console.WriteLine("Task id={0},Thread id={1},Context={2}",
+                Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
+                SynchronizationContext.Current);
+            Console.WriteLine("...Waiting task completion...");
+            ret = t2.Result;
+            Console.WriteLine("...Task Complete:{0}...", ret);
+
 
             Console.ReadLine();
 
