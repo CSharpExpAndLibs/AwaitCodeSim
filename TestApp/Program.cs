62f2cacbe07981d0daffbaf48ee29c54b3c93fb4
sak52jp <charan.49jp@gmail.com>
2020-06-07 20:35:24 +0900

    NLOGのトライアル

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using NLog;
 
 
 namespace TestApp
 {
     class AwaitStructure
     {
+        //private static Logger logger = LogManager.GetCurrentClassLogger();
+
         public static async Task HeavyWork()
         {
             Console.WriteLine("--Enter HeavyWork()--");
@@ -51,6 +54,7 @@ namespace TestApp
 
     class AwaitCodeSim
     {
+        //private static Logger logger = LogManager.GetCurrentClassLogger();
 
         public static Task HeavyWork()
         {
@@ -138,6 +142,9 @@ namespace TestApp
 
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        //private static Logger logger = null;
+
         public static void HeavyMethod()
         {
             Console.WriteLine("---Enter HeavyMethod()---");
@@ -157,59 +164,87 @@ namespace TestApp
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Main()");
+#if false
+            if (logger == null)
+            {
+                LogManager.Configuration = new NLog.Config.XmlLoggingConfiguration(
+                    @"C:\Users\chara\source\repos\TestApp2\NLog.config");
+                logger = LogManager.GetCurrentClassLogger();
+            }
+#endif // false
+            while (true)
+            {
+                logger.Info("Enter Main() {Name}", "Sakamoto");
 
-            Console.WriteLine("Task id={0},Thread id={1},Context={2}",
-                Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
-                SynchronizationContext.Current);
+                Console.WriteLine("Enter Main()");
 
-            Console.WriteLine("Before call AwaitStructure.HeavyWork()");
-            Task t = AwaitStructure.HeavyWork();
-            Console.WriteLine("Return from AwaitStructure.HeavyWork() [tid={0}]", t.Id);
-            Console.WriteLine("Task id={0},Thread id={1},Context={2}",
-                Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
-                SynchronizationContext.Current);
-            Console.WriteLine("...Waiting task completion...");
-            t.Wait();
-            Console.WriteLine("...Task Complete...");
-            Console.WriteLine("===============================================");
+                Console.WriteLine("Task id={0},Thread id={1},Context={2}",
+                    Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
+                    SynchronizationContext.Current);
 
-            Console.WriteLine("Before call AwaitCodeSim.HeavyWork()");
-            t = AwaitCodeSim.HeavyWork();
-            Console.WriteLine("Return from AwaitCodeSim.HeavyWork() [tid={0}]", t.Id);
-            Console.WriteLine("Task id={0},Thread id={1},Context={2}",
-                Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
-                SynchronizationContext.Current);
-            Console.WriteLine("...Waiting task completion...");
-            t.Wait();
-            Console.WriteLine("...Task Complete...");
+                Console.WriteLine("Before call AwaitStructure.HeavyWork()");
+                Task t = AwaitStructure.HeavyWork();
+                Console.WriteLine("Return from AwaitStructure.HeavyWork() [tid={0}]", t.Id);
+                Console.WriteLine("Task id={0},Thread id={1},Context={2}",
+                    Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
+                    SynchronizationContext.Current);
+                Console.WriteLine("...Waiting task completion...");
+                t.Wait();
+                Console.WriteLine("...Task Complete...");
+                Console.WriteLine("===============================================");
 
-            Console.WriteLine("===============================================");
-            string ret = null;
-            Console.WriteLine("Before call AwaitStructure.HeavyWorkWithReturn()");
-            Task<string> t2 = AwaitStructure.HeavyWorkWithReturn();
-            Console.WriteLine("Return from AwaitStructure.HeavyWorkWithReturn() [tid={0}]", t.Id);
-            Console.WriteLine("Task id={0},Thread id={1},Context={2}",
-                Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
-                SynchronizationContext.Current);
-            Console.WriteLine("...Waiting task completion...");
-            ret = t2.Result;
-            Console.WriteLine("...Task Complete:{0}...", ret);
-            Console.WriteLine("===============================================");
+                Console.WriteLine("Before call AwaitCodeSim.HeavyWork()");
+                t = AwaitCodeSim.HeavyWork();
+                Console.WriteLine("Return from AwaitCodeSim.HeavyWork() [tid={0}]", t.Id);
+                Console.WriteLine("Task id={0},Thread id={1},Context={2}",
+                    Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
+                    SynchronizationContext.Current);
+                Console.WriteLine("...Waiting task completion...");
+                t.Wait();
+                Console.WriteLine("...Task Complete...");
 
-            ret = null;
-            Console.WriteLine("Before call AwaitCodeSim.HeavyWorkWithReturn()");
-            t2 = AwaitCodeSim.HeavyWorkWithReturn();
-            Console.WriteLine("Return from AwaitCodeSim.HeavyWorkWithReturn() [tid={0}]", t.Id);
-            Console.WriteLine("Task id={0},Thread id={1},Context={2}",
-                Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
-                SynchronizationContext.Current);
-            Console.WriteLine("...Waiting task completion...");
-            ret = t2.Result;
-            Console.WriteLine("...Task Complete:{0}...", ret);
+                Console.WriteLine("===============================================");
+                string ret = null;
+                Console.WriteLine("Before call AwaitStructure.HeavyWorkWithReturn()");
+                Task<string> t2 = AwaitStructure.HeavyWorkWithReturn();
+                Console.WriteLine("Return from AwaitStructure.HeavyWorkWithReturn() [tid={0}]", t.Id);
+                Console.WriteLine("Task id={0},Thread id={1},Context={2}",
+                    Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
+                    SynchronizationContext.Current);
+                Console.WriteLine("...Waiting task completion...");
+                ret = t2.Result;
+                Console.WriteLine("...Task Complete:{0}...", ret);
+                Console.WriteLine("===============================================");
 
+                ret = null;
+                Console.WriteLine("Before call AwaitCodeSim.HeavyWorkWithReturn()");
+                t2 = AwaitCodeSim.HeavyWorkWithReturn();
+                Console.WriteLine("Return from AwaitCodeSim.HeavyWorkWithReturn() [tid={0}]", t.Id);
+                Console.WriteLine("Task id={0},Thread id={1},Context={2}",
+                    Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
+                    SynchronizationContext.Current);
+                Console.WriteLine("...Waiting task completion...");
+                ret = t2.Result;
+                Console.WriteLine("...Task Complete:{0}...", ret);
 
-            Console.ReadLine();
+                Console.WriteLine("#### Input exit to finish or just enter to continue. ####");
+
+            
+                string str = Console.ReadLine();
+                if (str == "exit")
+                    break;
+
+                /* Reload Nlog.config
+                 * loggerをcreateした後でもconfigは変え放題であることが分かった。
+                 * なので、Terminal毎に変更IFを準備したら、ランタイムでConfig.XXX
+                 * を変更すれば幾らでも再構成可能。
+                 * 下記例は、コンフィグファイルを再読み込みしているが、APIを使って
+                 * プログラムで処理しても全く同じことが可能。
+                 */
+                LogManager.Configuration = new NLog.Config.XmlLoggingConfiguration(
+                    @"C:\Users\chara\source\repos\TestApp2\TestApp\NLog.config");
+
+            }
 
         }
 
